28ad662cbdca77023d42c47ac8cd4c6b3e2a7532
gabrielgiroe1 <gabriel.giroe@gmail.com>
2022-05-24 23:33:06 +0300

    Review animal.rb

--- animal/animal.rb
+++ animal/animal.rb
@@ -1,15 +1,5 @@
-module Mammal
   class Animal
     def speak(sound)
       p "#{sound}"
     end
   end
-  end
-
-
-buddy = Mammal::Dog.new
-kitty = Mammal::Cat.new
-buddy.speak('Arf!')           # => "Arf!"
-kitty.say_name('kitty')       # => "kitty"
-p buddy
-p kitty
